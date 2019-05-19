bb473784d9edc3cea9797e7eedae0ca71138317f
Erica Hawkins <ehawk901@gmail.com>
2019-05-19 16:58:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ def display_board (board)
 puts["board"]
 puts("board[0]","board[1]","board[3]")
 puts( "-----------")
-("board[4]","board[5", "board[6]")
-("-----------")
+puts("board[4]","board[5", "board[6]")
+puts("-----------")
 ()
 end 
