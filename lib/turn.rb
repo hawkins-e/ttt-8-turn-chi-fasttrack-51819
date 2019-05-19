caf10b5f3b85d3b49aa11bd8d012dc8b5c53a039
Erica Hawkins <ehawk901@gmail.com>
2019-05-19 17:17:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,5 +4,5 @@ puts"#{board[0]},|,#{board[1]},|,#{board[3]}"
 puts"#{-----------}"
 puts"#{board[4]},|,#{board[5]},|,#{board[6]}"
 puts"-----------"
-puts"#{board[7]},|,#{board[8]},| ,#{board[9]"
+puts"#{board[7]},|,#{board[8]},|,#{board[9]}"
 end 
